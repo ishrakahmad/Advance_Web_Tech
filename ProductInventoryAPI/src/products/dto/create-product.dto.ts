8c8830c88906a0bcc1c0f3b19861d5e7076c1c3a
Ishrak Ahmad <ishrakahmad782@gmail.com>
2026-07-11 00:56:29 +0600

    validation

--- ProductInventoryAPI/src/products/dto/create-product.dto.ts
+++ ProductInventoryAPI/src/products/dto/create-product.dto.ts
@@ -1,16 +1,23 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateProductDto {
 
-    // Product er name nibo
-    productName: string;
-  
-    // Brand er name nibo
-    brand: string;
-  
-    // Product er price nibo
-    price: number;
-  
-    // Product er quantity nibo
-    quantity: number;
-    
-  
-  }
+  // Product er name empty hote parbe na
+  @IsString()
+  @IsNotEmpty()
+  productName: string;
+
+  // Brand er name empty hote parbe na
+  @IsString()
+  @IsNotEmpty()
+  brand: string;
+
+  // Price number hote hobe
+  @IsNumber()
+  price: number;
+
+  // Quantity number hote hobe
+  @IsNumber()
+  quantity: number;
+
+}
